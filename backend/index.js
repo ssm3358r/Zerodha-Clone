d174b53fa13c4c7e00dc310ad793c1b9faa101c9
Sourabh <malviya.sourabh.124@gmail.com>
2026-01-04 01:12:16 +0530

    seccure true in session

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none", // REQUIRED for Netlify ↔ Render
       maxAge: 7 * 24 * 60 * 60 * 1000,
     },
